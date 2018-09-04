66b7c525fe3d3a9f7e5d476cebc66208f2af00a8
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 17:14:31 +0200

    display meals in a table with semantic ui react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Container, Table } from 'semantic-ui-react'
 import './App.css'
 import Meal from './components/Meal.js'
 import Header from './components/Header.js'
@@ -8,7 +9,7 @@ class App extends Component {
     meals: []
   }
 
-  constructor() {
+  constructor () {
     super()
 
     fetch('http://localhost:4000/meals/1')
@@ -18,16 +19,25 @@ class App extends Component {
       })
   }
 
-  render() {
+  render () {
     return (
       <div className="App">
-        <h1>La carte</h1>
-        <table>
-          <tbody>
-            {this.state.meals.map(m => <Meal key={m.id} meal={m} />)}
-          </tbody>
-        </table>
+        <Container fluid>
           <Header />
+          <h2>La carte</h2>
+          <Table celled striped>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell colSpan='3' style={{ fontFamily: 'Nothing You Could Do, cursive', fontSize: '24px', height: '50px', paddingBottom: '20px' }}>
+                  Nos plats
+                </Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.state.meals.map(m => <Meal key={m.id} meal={m} />)}
+            </Table.Body>
+          </Table>
+        </Container>
       </div>
     )
   }
