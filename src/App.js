07036b57233dd8306dca81696c7eadf87167d1a8
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 14:07:31 +0200

    first display of meals

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import React, { Component } from 'react'
 import './App.css'
+import Meal from './components/Meal.js'
 
 class App extends Component {
-  render () {
-state = {
+  state = {
     meals: []
   }
+
   constructor() {
     super()
+
     fetch('http://localhost:4000/meals/1')
       .then(res => res.json())
-      .then(console.log)
       .then(meals => {
         this.setState({ meals: meals })
       })
   }
+
+  render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <h1>La carte</h1>
+        <table>
+          <tbody>
+            {this.state.meals.map(m => <Meal key={m.id} meal={m} />)}
+          </tbody>
+        </table>
       </div>
     )
   }
