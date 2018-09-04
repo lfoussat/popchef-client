b7526b30d34b5195e4737fa1d2c386a2842d4217
Louise Foussat <louisefoussat@gmail.com>
2018-09-04 12:33:04 +0200

    add lint

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <h1>Hello</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
