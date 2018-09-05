2b3b8f09e4bcddfa845f836a9f4d1c8e1157d797
Louise Foussat <louisefoussat@gmail.com>
2018-09-05 13:30:21 +0200

    fix

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const api = 'https://popchef-server.herokuapp.com/'
+const api = 'https://popchef-server.herokuapp.com'
 
 export const getMeals = () => fetch(`${api}/meals/1`)
   .then(res => res.json())
