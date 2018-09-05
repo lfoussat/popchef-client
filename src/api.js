c71625192bd5beae072fe661589f46079b61ad67
Louise Foussat <louisefoussat@gmail.com>
2018-09-05 13:22:04 +0200

    api heroku

--- src/api.js
+++ src/api.js
@@ -1,6 +1,4 @@
-const api = window.location.url.includes('localhost')
-  ? 'http://localhost:4000'
-  : 'https://popchef-server.herokuapp.com/'
+const api = 'https://popchef-server.herokuapp.com/'
 
 export const getMeals = () => fetch(`${api}/meals/1`)
   .then(res => res.json())
